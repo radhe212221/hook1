74151199b70100be33097cdeebdc3fcc8ae48808
radhe212221 <radhe212221@gmail.com>
2022-05-25 21:10:26 +0530

    added counter example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
-
+import React, { useState } from 'react';
+import './style.css';
+//use state
 export default function App() {
+  const [s, sets] = useState(0);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>s: {s}</h1>
+      <button onClick={() => sets(s + 1)}>+</button>
+      <button onClick={() => sets(s - 1)}>-</button>
+      <button onClick={() => sets(0)}>0</button>
     </div>
   );
 }
