ba3e2802ad84dc5e4493ba2bd7a6183ad0acb0a1
radhe212221 <radhe212221@gmail.com>
2022-05-25 21:30:47 +0530

    state with add more example

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import React, { useState } from 'react';
 import './style.css';
-//use state
-export default function App() {
-  const [topics, settopics] = useState([]);
 
-  const addtopic = () => {
-    const mytopic = prompt('enter topic name ', 'html topic');
-    settopics([...topics, mytopic]);
+export default function App() {
+  const intialStock = {
+    item: '',
+    price: 0,
+    qty: 0,
   };
+  const [stock, setstock] = useState([intialStock]);
+
+  const addmore = () => {
+    setstock([...stock, intialStock]);
+  };
+  const handleChange = (i, name, e) => {
+    let value = e.target.value;
+    setstock(
+      stock?.map((x, index) => (index === i ? { ...x, [name]: value } : x))
+    );
+  };
+
+  const totalPrice = stock
+    .map((x) => +x.price)
+    .reduce((sum, next) => sum + next);
+
+  const totalQty = stock.map((x) => +x.qty).reduce((sum, next) => sum + next);
+
   return (
     <div>
-      <button onClick={addtopic}>add topics </button>
-      <h1>All topics : {topics?.length}</h1>
-      {topics?.map((item) => (
-        <li>{item}</li>
+      {stock?.map((item, index) => (
+        <div className="stock-item">
+          <h3>item : {index} </h3>
+          <input
+            onChange={(e) => handleChange(index, 'item', e)}
+            value={item?.item}
+            placeholder={'enter item :' + index}
+          />
+          <input
+            onChange={(e) => handleChange(index, 'price', e)}
+            value={item?.price}
+            placeholder={'enter price :' + index}
+          />
+          <input
+            onChange={(e) => handleChange(index, 'qty', e)}
+            value={item?.qty}
+            placeholder={'enter qty :' + index}
+          />
+        </div>
       ))}
+      <button onClick={addmore}>add more </button>
+      <h1>
+        totalItems: {stock.length} {'  '}
+        totalPrice: {totalPrice} {'  '}
+        totalQty: {totalQty} {'  '}
+      </h1>
     </div>
   );
 }
